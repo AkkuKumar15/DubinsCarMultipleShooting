e70b4a3e4d8e050c26a1cb477befac426d7a5904
AkkuKumar15 <78607586+AkkuKumar15@users.noreply.github.com>
2023-09-01 17:35:44 -0500

    Update shooting_method_dubins_car.py

--- shooting_method_dubins_car.py
+++ shooting_method_dubins_car.py
@@ -4,16 +4,17 @@ import matplotlib.pyplot as plt
 import time
 from math import *
 
-# Constants
-linear_velocity = 1.0
+# Parameters
+linear_velocity = 1.0 # Constant velocity
 dt = 0.1  # Time step
-x_initial = 1
-y_initial = 5
-x_final = 5
-y_final = 10
-turn_radius = 1
-theta_initial = 0
+x_initial = 1 # Initial x
+y_initial = 5 # Initial y
+x_final = 5 # Desire x
+y_final = 10 # Desired y
+turn_radius = 1 # Constant turn radius
+theta_initial = 0 # Initial heading
 
+# Car Dynamics
 def dubins_car_model(state, angular_velocity):
     x, y, theta = state
     x_next = x + linear_velocity * np.cos(theta) * dt
@@ -21,6 +22,7 @@ def dubins_car_model(state, angular_velocity):
     theta_next = theta + (linear_velocity * np.tan(angular_velocity) / turn_radius) * dt
     return np.array([x_next, y_next, theta_next])
 
+# Car Trajectories
 def trajectory_positions(angular_velocities):
     state = np.array([x_initial, y_initial, theta_initial])  # Initial state
     positions = [state[:2]]
@@ -31,13 +33,14 @@ def trajectory_positions(angular_velocities):
     
     return positions
 
+# Objective Function
 def objective_function(angular_velocities):
     positions = trajectory_positions(angular_velocities)
     final_position = positions[-1]
     final_position_cost = np.linalg.norm(final_position - [x_final, y_final])
     return final_position_cost
 
-# Initial guess for angular velocities
+# Number of Steps and Initial Guess for Angular Velocities
 num_steps = 100
 initial_guess = np.zeros(num_steps)
 
@@ -54,7 +57,7 @@ print("Total Time of Execution =", (final_time-initial_time)/ (10 ** 9), "second
 optimal_angular_velocities = result.x
 optimal_positions = trajectory_positions(optimal_angular_velocities)
 
-# Plot the trajectory
+# Plot the trajectory in the x-y plane
 x_values = [pos[0] for pos in optimal_positions]
 y_values = [pos[1] for pos in optimal_positions]
 
